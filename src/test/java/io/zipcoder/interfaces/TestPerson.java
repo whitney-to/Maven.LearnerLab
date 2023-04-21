8dff3f1dc15be3b29ac34ed4f415cf29d9528361
whitney-to <whitney012923@gmail.com>
2023-04-20 22:52:40 -0400

    adde Person class and constructor test

--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -1,5 +1,20 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestPerson {
 
+    @Test
+    public void testConstructor(){
+        Person person = new Person(2897124l,"BOO");
+        Long expected = 2897124l;
+        String expectedName = "BOO";
+
+        Long actual = person.getId();
+        String actualName = person.getName();
+
+        Assert.assertEquals(expected,actual);
+        Assert.assertEquals(expectedName,actualName);
+    }
 }
